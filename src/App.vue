b1c44c2070aff2a08bc6150915eb89d8c8e8bda0
SeanBecker <techtronixwebdesign@gmail.com>
2018-11-24 21:38:18 -0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <h1>Animations</h1>
+                <hr>
+                <button class="btn btn-primary mb-3" @click="show = !show">Show Alert</button>
+                <transition>
+                    <div class="alert alert-info" v-if="show">This is an alert box!</div> 
+                </transition>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                show: false
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>
