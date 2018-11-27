5ec9267e7a140700a324f7c0229bba40f9800a06
SeanBecker <techtronixwebdesign@gmail.com>
2018-11-26 23:04:28 -0600

    more animation stuff - transitions between multiple elements

--- src/App.vue
+++ src/App.vue
@@ -5,32 +5,42 @@
                 <h1>Vue.js Animations</h1>
                 <hr>
                 <select class="form-control" v-model="selectedAnimation">
-                    <option value="fade">Downwards Fade</option>
                     <option value="slide-left">Slide Left</option>
                     <option value="slide-right">Slide Right</option>
                 </select>
                 <hr>
                 <button class="btn btn-primary mb-3" @click="show = !show">Show Alert Animations</button>
+                <transition
+                    enter-active-class="animated pulse"
+                    leave-active-class="animated pulse">
+                    <div class="alert alert-warning" v-if="show">AnimateCSS Animations</div>
+                </transition>
                 <transition name="fade" type="transition">
-                    <div class="alert alert-primary" v-if="show">This is an alert box!</div> 
+                    <div class="alert alert-primary" v-if="show"> an alert box using the transition class anim hooks</div> 
                 </transition>
                 <transition name="slide-left" type="animation">
                     <div class="alert alert-info" v-if="show">
                         <h2 class="mb-1">This is an alert box!</h2>
                     </div> 
                 </transition>
-                <transition name="slide-right" type="animation" appear>
-                    <div class="alert alert-info" v-if="show">OFF</div> 
-                    <div class="alert alert-primary" v-else>This is an alert box DANGER op.</div> 
-                </transition>
-                <transition
-                    enter-active-class="animated pulse"
-                    leave-active-class="animated pulse">
-                    <div class="alert alert-warning" v-if="show">Some Cool Animations</div>
-                </transition>
+                
                 
             </div>
-            <!-- <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 mt-5 pt-4">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 mt-5 pt-4">
+                <!-- 
+                    transition between different elements 
+                    always check the type against the action
+                    set the mode for when to trigger next element animation
+                -->
+                <transition :name="selectedAnimation" type="animation" mode="out-in">
+                    <div class="alert alert-info" v-if="show" key="off">OFF</div> 
+                    <div class="alert alert-primary" v-else key="on">ON</div> 
+                </transition>
+                <transition name="fade" type="transition" mode="out-in">
+                    <div class="alert alert-info" v-if="show" key="off2">OFF</div> 
+                    <div class="alert alert-primary" v-else key="on2">ON</div> 
+                </transition>
+
                 <div class="card">
                     <h5 class="card-header">Animation with Vue</h5>
                     <div class="card-body expand-body" v-if="show">
@@ -43,7 +53,7 @@
                         </ul>
                     </div>
                 </div>
-            </div> -->
+            </div>
         </div>
     </div>
 </template>
@@ -77,6 +87,9 @@ body {
 }
 .alert-info {
     background-color: rgba(3, 216, 203, .2);
+}
+.alert-warning {
+    background-color: rgb(245, 125, 125);
 }
 
 .card-header {
